ce4f931bf78c7e52cff7cf38636c598a307f8cc3
tusharrgoel <tushar.goel@practo.com>
2024-06-26 17:50:48 +0530

    sentry integration checked

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
+
 function App() {
+  const throwError = () => {
+    throw new Error("This is a test error to check the Sentry integration!");
+  };
+
   return (
     <div className="App">
-      <p>Hello and Welcome to Practo's Assignment 3 i.e Sentry</p>
-      <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+      <header className="App-header">
+        <h1>Sentry Integration Test with a simple react application</h1>
+        <button onClick={throwError}>Throw Error</button>
+      </header>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,2 @@
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="da39a3ee-5e6b-5b0d-b255-bfef95601890")}catch(e){}}();
+//# debugId=da39a3ee-5e6b-5b0d-b255-bfef95601890
