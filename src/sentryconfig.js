f87e0b0eb58fce5cc7a71ae19d35a8bcf1166fb7
tusharrgoel <tushar.goel@practo.com>
2024-06-26 16:47:41 +0530

    sentry assignment

--- src/sentryconfig.js
+++ src/sentryconfig.js
@@ -1,6 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+//...
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
@@ -17,9 +15,7 @@ Sentry.init({
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const container = document.getElementById(“app”);
+const root = createRoot(container);
+root.render(<App />);
