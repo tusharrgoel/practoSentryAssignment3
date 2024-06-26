c40d0152b7511a32dcefb698aa5c67180d52274a
tusharrgoel <tushar.goel@practo.com>
2024-06-26 16:52:04 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="App">
       <p>Hello and Welcome to Practo's Assignment 3 i.e Sentry</p>
+      <button onClick={() => methodDoesNotExist()}>Break the world</button>;
     </div>
   );
 }
